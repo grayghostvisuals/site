dfc6108fb9b39e62c8df01ea100efa0adb57a544
grayghostvisuals <grayghost@grayghostvisuals.com>
2015-05-26 16:45:09 -0400

    Revert "only use required fonts"
    This reverts commit bea14b05ba754a5bb2e988dfda64ef5beeed5a3a.

--- site/js/src/ffobserver.js
+++ site/js/src/ffobserver.js
@@ -7,11 +7,10 @@
   var fontA = new w.FontFaceObserver('Avenir LT W01_45 Book'),
       fontB = new w.FontFaceObserver('Ciutadella W04 Lt'),
       fontC = new w.FontFaceObserver('Ciutadella W04 SmBold'),
-      fontD = new w.FontFaceObserver('Ciutadella W04 Medium'),
-      fontE = new w.FontFaceObserver('Ciutadella W04 Bold');
+      fontD = new w.FontFaceObserver('Ciutadella W04 Bold');
 
   w.Promise
-    .all([fontA.check(), fontB.check(), fontC.check(), fontD.check(), fontE.check()])
+    .all([fontA.check(), fontB.check(), fontC.check(), fontD.check()])
     .then(function(){
       w.document.documentElement.className += ' ffo-loaded';
     });
