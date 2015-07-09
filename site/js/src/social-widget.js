e75c539bcb2715b4259007de0f7b6676faafd2fb
grayghostvisuals <grayghost@grayghostvisuals.com>
2015-07-09 16:40:28 -0400

    organization

--- site/js/src/social-widget.js
+++ site/js/src/social-widget.js
@@ -1,7 +1,9 @@
 var circle = document.querySelectorAll('div.socials__btn');
 
+function socialBloom() {
+  this.parentNode.classList.toggle('is-bloomed');
+}
+
 for(var i = 0, l = circle.length; i < l; i++) {
-  circle[i].addEventListener('click', function() {
-    this.parentNode.classList.toggle('is-bloomed');
-  });
+  circle[i].addEventListener('click', socialBloom);
 }
